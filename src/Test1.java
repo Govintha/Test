1cc9f3dbaef1ca09f36abb212e277553237ba99b
Govinth <govinth277@gmail.com>
2024-04-19 04:22:39 +0530

    first commit

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		
+		
+	}
+	
+}
